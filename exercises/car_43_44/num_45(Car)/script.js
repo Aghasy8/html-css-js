187e64f8de5ab5cdc33153a71686623a8a0db66d
Aghasy8 <107751705+Aghasy8@users.noreply.github.com>
2022-10-02 18:59:34 +0400

    Update script.js

--- exercises/car_43_44/num_45(Car)/script.js
+++ exercises/car_43_44/num_45(Car)/script.js
@@ -1,11 +1,14 @@
+// 45. Շատ շատ բարդ աստիճան։ Աջ կամ Ձախ սեղմելիս
+// ավտոմեքենան սկսի պտույտ կատարել համապատասխան
+// ուղղությում, իսկ առաջ կամ հետ սեղմելիս այդ թեքման
+//  անկյունով առաջ կամ հետ գնա
+
 const car = document.querySelector(".car");
 const speed = 10;
-const leftDegree = "0deg";
-const rightDegree = "180deg";
-const upDegree = "90deg";
-const downDegree = "-90deg";
+const angleChange = 5;
 
-// car.style.transform = `rotate(${degree})`;
+let numOfHitsRight = 0;
+let numOfHitsLeft = 0;
 
 window.addEventListener("load", () => {
   car.style.position = "absolute";
@@ -15,21 +18,62 @@ window.addEventListener("load", () => {
 
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    case "ArrowLeft":
-      car.style.transform = `rotate(${leftDegree})`;
-      car.style.left = parseInt(car.style.left) - speed + "px";
-      break;
     case "ArrowRight":
-      car.style.transform = `rotate(${rightDegree})`;
-      car.style.left = parseInt(car.style.left) + speed + "px";
+      if (numOfHitsRight === 360 / angleChange - 1) {
+        numOfHitsRight = 0;
+      } else {
+        numOfHitsRight++;
+      }
+
+      car.style.transform = `rotate(${numOfHitsRight * angleChange}deg)`;
+      numOfHitsLeft = numOfHitsRight;
       break;
-    case "ArrowUp":
-      car.style.transform = `rotate(${upDegree})`;
-      car.style.top = parseInt(car.style.top) - speed + "px";
-      break;
-    case "ArrowDown":
-      car.style.transform = `rotate(${downDegree})`;
-      car.style.top = parseInt(car.style.top) + speed + "px";
+
+    case "ArrowLeft":
+      if (numOfHitsLeft === 360 / angleChange - 1) {
+        numOfHitsLeft = 0;
+      } else {
+        numOfHitsLeft--;
+      }
+
+      car.style.transform = `rotate(${numOfHitsLeft * angleChange}deg)`;
+      numOfHitsRight = numOfHitsLeft;
       break;
   }
 });
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("keydown", (event) => {
+//   switch (event.key) {
+//     case "ArrowLeft":
+//       car.style.transform = `rotate(${leftDegree})`;
+//       car.style.left = parseInt(car.style.left) - speed + "px";
+//       break;
+//     case "ArrowRight":
+//       car.style.transform = `rotate(${rightDegree})`;
+//       car.style.left = parseInt(car.style.left) + speed + "px";
+//       break;
+//     case "ArrowUp":
+//       car.style.transform = `rotate(${upDegree})`;
+//       car.style.top = parseInt(car.style.top) - speed + "px";
+//       break;
+//     case "ArrowDown":
+//       car.style.transform = `rotate(${downDegree})`;
+//       car.style.top = parseInt(car.style.top) + speed + "px";
+//       break;
+//   }
+// });
+
+
+
+
+// car.style.transform = `rotate(${leftDegree})`;
+// car.style.left = parseInt(car.style.left) - speed + "px";
