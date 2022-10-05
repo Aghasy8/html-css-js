52a9d10af53a325500dc387e6d2d2a5acfd64d24
Aghasy8 <107751705+Aghasy8@users.noreply.github.com>
2022-10-05 13:33:56 +0400

    Update script.js

--- exercises/num_42/script.js
+++ exercises/num_42/script.js
@@ -9,6 +9,9 @@ const password = document.getElementById("password");
 const loginError = document.createElement("span");
 const passwordError = document.createElement("span");
 const container = document.getElementById("container");
+const containerLogin = document.getElementById("containerLogin");
+const containerPassword = document.getElementById("containerPassword");
+
 container.style.color = "red";
 function onlySpaces(str) {
   return str.trim().length === 0;
@@ -26,7 +29,7 @@ function check() {
     login.value.length > 20
   ) {
     login.style.border = "solid red";
-    container.append(loginError);
+    containerLogin.append(loginError);
   }
 
   if (login === "") {
@@ -41,7 +44,7 @@ function check() {
 
   if (password.value === "" || onlySpaces(password.value) === true) {
     password.style.border = "solid red";
-    container.append(passwordError);
+    containerPassword.append(passwordError);
   }
 
   if (password.value === "") {
@@ -49,30 +52,19 @@ function check() {
   } else if (onlySpaces(password.value) === true) {
     passwordError.innerText = `Password can't contain only spaces`;
   }
+
+  if (
+    login !== "" &&
+    login.value.search(" ") === -1 &&
+    login.value.length >= 3 &&
+    login.value.length <= 20
+  ) {
+    login.style.border = null;
+    loginError.remove();
+  }
+
+  if (password.value !== "" && onlySpaces(password.value) !== true) {
+    password.style.border = null;
+    passwordError.remove();
+  }
 }
-
-//  if (password.value === "") {
-//    password.style.border = "solid red";
-//    body.append(error);
-//    error.innerText = "Password can't be empty";
-//  } else if (onlySpaces(password.value) === true) {
-//    password.style.border = "solid red";
-//    body.append(error);
-//    error.innerText = "Password can't contain only spaces";
-//  }
-
-//function check() {
-//   if (login === "") {
-//     error.innerText = "Login can't be empty";
-//     body.append(error);
-//   } else if (login.value.search(" ") !== -1) {
-//     error.innerText = "Login can't contain any space";
-//     body.append(error);
-//   } else if (onlySpaces(login.value) === true) {
-//     error.innerText = "Login can't contain only spaces";
-//     body.append(error);
-//   } else if (login.value.length < 3 || login.value.length > 20) {
-//     error.innerText = "Login can't contain less than 3 or more than 20 symbols";
-//     body.append(error);
-//   }
-// }
